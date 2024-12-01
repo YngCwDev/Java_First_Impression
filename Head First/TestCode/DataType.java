8d8405f7e5704e400220848df15dae48614e1282
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2024-12-01 23:57:12 +0200

    Checkpoint

---
+++ Head First/TestCode/DataType.java
@@ -0,0 +1,17 @@
+
+class TestingDataTypes{
+
+	void sayHello(){
+		System.out.println("Hello");	
+	}
+}
+
+
+
+
+class DataType{
+	public static void main(String[] args){
+	x = new TestingDataTypes();
+	x.sayHello;	
+	}
+}	
